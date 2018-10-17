1bff5e77b0c144e5925df2e8c6c333bae7766470
CarolinaMC <carmc06@gmail.com>
2018-10-16 23:17:37 -0600

    lambda a la izq2

--- funcionesAlgoritmo.py
+++ funcionesAlgoritmo.py
@@ -103,8 +103,6 @@ def extraerreglas(grammar):
             for matchobj in re.finditer(syntaxre, grammar)#Encuentre todas las subcadenas donde coincida la RE, y las devuelve como un iterador.
             if matchobj.group('rule')]#va metiedo a la lista si cumple lo de la reglas Ejm:[('"A"',apple,False)] Devuelve la cadena emparejada por el RE
 def remplazarReglas(self,text, grammar):
-    listaSim = list(self.symbolsEdit.text())
-    listaMar = list(self.markersEdit.text())
     while True:
         for pat, repl, term in grammar:
             if pat == "Λ":
